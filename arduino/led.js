0814c438fe2045a337226ef43be10b730db831c1
scottmotte <scott@scottmotte.com>
2014-03-13 18:31:23 -0700

    200 ms

--- arduino/led.js
+++ arduino/led.js
@@ -4,5 +4,6 @@ var board   = new five.Board();
 board.on("ready", function() {
   // Create an Led on pin 13 and strobe it on/off
   // Optionally set the speed; defaults to 100ms
-  (new five.Led(13)).strobe(2500);
+  var led = new five.Led(13);
+  led.strobe(200);
 });
